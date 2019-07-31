579eff3002cde55acd1ea42882dbfc53911d7ccb
xiekecheng <568835977@qq.com>
2019-07-31 14:12:06 +0800

    swagger test

---
+++ swagger2test/src/main/java/com/learn/Swagger2testApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2testApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger2testApplication.class, args);
+    }
+
+}
